104df08d405f0fd6c48064cb2059ee6d070da166
mintonlindley <mintonlindley@gmail.com>
2025-12-03 12:13:41 -0500

    Update RulesPage.java

--- src/RulesPage.java
+++ src/RulesPage.java
@@ -16,7 +16,17 @@ public class RulesPage extends JFrame {
         txt.setEditable(false);
         txt.setLineWrap(true);
         txt.setWrapStyleWord(true);
-        txt.setText("Rules will go here."); 
+        txt.setText("TEXAS HOLD'EM RULES\n" +
+            "Each player is dealt two private cards (hole cards).\n" +
+            "Five community cards are dealt face up in three stages (flop, turn, and river).\n" +
+            "Players make the best five-card hand using any combination of community cards and their hole cards.\n" +
+            "Betting rounds occur.\n" +
+            "BLACKJACK RULES\n" +
+            "Each player is dealt two cards. Dealer gets two cards.\n" +
+            "The goal is to get as close to 21 as possible without going over.\n" +
+            "Dealer must hit until at least 17.\n" +
+            "If you exceed 21, you bust and lose immediately.\n" +
+            "If neither busts, higher total wins.\n"); 
         JScrollPane sp = new JScrollPane(txt);
         add(sp, BorderLayout.CENTER);
         setVisible(true);
